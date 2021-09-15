68815afd4c505e4f924360bbd449a992c14103c1
Luke Briggs <lukebriggs02@gmail.com>
2021-09-15 19:28:33 +0100

    Re-add sleep interval to work on chrome

--- bbc2spotify.js
+++ bbc2spotify.js
@@ -1,21 +1,27 @@
 console.log("LOADED BBC2SPOTIFY ON " + window.location.hostname);
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function main(){
 
     // List of buttons that bring up track popover
     var list = document.querySelectorAll('[id^="track-"]')
     var tracklist = {}
     var episodeDate = document.getElementsByClassName("sc-c-episode__metadata__data")[0].lastChild.data
-    
+
+    var t = 200
+
     for (var i=0; i < list.length; i++){
         list[i].scrollIntoView()
         list[i].click()
+        await sleep(t)
         try{
             var track = document.querySelectorAll('[href^="https://open.spotify.com/track/"]')[0]
             var trackMetadata = JSON.parse(track.getAttribute("data-bbc-metadata"))
             var trackTitle = trackMetadata.TID
-            var trackLink = track.getAttribute("href")
-            tracklist[trackTitle] = trackLink
+            tracklist[trackTitle] = track.getAttribute("href")
         } catch {
             // No links found
         }
